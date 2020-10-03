01ad3cc7e0ea274abf357a0d629a4fbd8cb06036
vladyslavnUA <vladyslavn@yahoo.com>
2020-10-03 03:17:53 -0400

    urls

--- calendr/urls.py
+++ calendr/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cal.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
