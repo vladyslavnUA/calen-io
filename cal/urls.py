685bb50a2e5d1ad561c83ed99fd7a079ba3c0bf7
vladyslavnUA <vladyslavn@yahoo.com>
2020-12-27 00:47:01 -0500

    update

--- cal/urls.py
+++ cal/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'cal'
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('calendar/', views.CalendarView.as_view(), name='calendar'),
+    path('', views.CalendarView.as_view(), name='calendar'),
     path('event/new/', views.event, name='event_new'),
 	url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
     url(r'^event/(?P<event_id>\d+)/$', views.detail, name='event_detail'),
